f60a9d77bea8256aa2071a2bd9bc3701c8bd32d4
Richard Yu <ryu323@gmail.com>
2016-05-21 15:22:24 -0700

    [feature] populate listings table

--- client/app/listings/listingsCtrl.js
+++ client/app/listings/listingsCtrl.js
@@ -4,10 +4,15 @@ angular.module('adaApp').controller('ListingsCtrl', ListingsCtrl);
 function ListingsCtrl($scope, listingsFactory) {
   var vm = this;
   vm.name = "adsf";
-  vm.listings = [];
+  vm.violations = [];
 
   vm.populateListings = function() {
-    listingsFactory.fetchListings()
+    listingsFactory.fetchListings().then(function(res) {
+      console.log(res);
+      res.forEach(function(item) {
+        vm.violations.push(item);
+      })
+    })
   }
 
   vm.populateListings();
