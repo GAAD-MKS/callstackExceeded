78bf3c6617898cd52df08875fc9eec8b1d3c0ac6
bobbychong <bobbychong@gmail.com>
2016-05-21 17:15:15 -0700

    [feature]added style to application

--- server/controllers/listingCtrl.js
+++ server/controllers/listingCtrl.js
@@ -9,6 +9,27 @@ exports.addListing = function(req, res) {
     status: { type: String, default: 'pending' }
   });
 
+  function decodeBase64Image(dataString) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+      response = {};
+
+    if (matches.length !== 3) {
+      return new Error('Invalid input string');
+    }
+
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
+
+    return response;
+  }
+
+  var imageBuffer = decodeBase64Image(image);
+  console.log(imageBuffer);
+
+  require("fs").writeFile("../client/test.png", new Buffer(image,"base64"), function(err) {
+  console.log(err);
+});
+
   violation.save(function(err) {
     if(err) {
       throw err;
